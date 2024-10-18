91006528ddab5b3e97d9d0fc3011a09d7445f0e3
Momin Ahmad <momin.ahmad-ext@jci.com>
2024-10-18 16:41:18 +0530

    main.js is now modular

---
+++ example/main.js
@@ -0,0 +1,33 @@
+import DrawCanvasShapes from '../main.js';
+
+const canvas = document.getElementById('canvas');
+const colorPicker = document.getElementById('colorPicker');
+const gridSize = document.getElementById('gridSize');
+const showGrid = document.getElementById('showGrid');
+
+const drawer = new DrawCanvasShapes(canvas, colorPicker.value, false, parseInt(gridSize.value));
+
+drawer.drawings;
+
+const height = document.getElementById('height');
+const width = document.getElementById('width');
+
+height.addEventListener('change', (event) => {
+  drawer.setCanvasSize(parseInt(width.value), parseInt(event.target.value),);
+});
+
+width.addEventListener('change', (event) => {
+  drawer.setCanvasSize(parseInt(event.target.value), parseInt(height.value));
+});
+
+colorPicker.addEventListener('change', (event) => {
+  drawer.setColor(event.target.value);
+});
+
+gridSize.addEventListener('change', (event) => {
+  drawer.setGridSize(parseInt(event.target.value));
+});
+
+showGrid.addEventListener('change', (event) => {
+  drawer.toggleGrid(event.target.checked);
+});
--- main.js
+++ main.js
@@ -1,217 +1,250 @@
-/**
- * @type {HTMLCanvasElement}
- */
-const canvas = document.getElementById('canvas');
-
-/**
- * @type {CanvasRenderingContext2D}
- */
-const ctx = canvas.getContext('2d');
-
-canvas.height = 480;
-canvas.width = 480;
-
-/**
- * @type {Array<{x: number, y: number}>}
- */
-let points = [];
-
-/**
- * @type {Array<{points: Array<{x: number, y: number}>, color: string}>}
- */
-let polygons = [];
-let isDrawing = false;
-let isGridVisible = false;
-
-const CLICK_THRESHOLD = 20;
-const CROSS_ICON_SIZE = 10;
-
-const drawType = document.getElementById('drawType');
-const colorPicker = document.getElementById('colorPicker');
-const drawButton = document.getElementById('drawButton');
-const cancelButton = document.getElementById('cancelButton');
-const clearButton = document.getElementById('clearButton');
-const gridSizeSelect = document.getElementById('gridSize');
-const showGridButton = document.getElementById('showGridButton');
-
-drawButton.addEventListener('click', () => {
-    isDrawing = true;
-    drawButton.disabled = true;
-    cancelButton.disabled = false;
-    points = [];
-    canvas.style.cursor = 'crosshair';
-});
-
-cancelButton.addEventListener('click', () => {
-    isDrawing = false;
-    drawButton.disabled = false;
-    cancelButton.disabled = true;
-    points = [];
-    canvas.style.cursor = 'default';
-    redraw();
-});
-
-clearButton.addEventListener('click', () => {
-    polygons = [];
-    redraw();
-});
-
-showGridButton.addEventListener('click', () => {
-    isGridVisible = !isGridVisible;
-    if (isGridVisible) {
-        const gridSize = parseInt(gridSizeSelect.value);
-        drawGrid(gridSize);
-        showGridButton.textContent = 'Hide Grid';
-    } else {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        showGridButton.textContent = 'Show Grid';
-    }
-    redraw();
-});
-
-function drawGrid(gridSize) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-
-    ctx.beginPath();
-    for (let x = 0; x <= canvas.width; x += gridSize) {
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, canvas.height);
+export default class DrawCanvasShapes {
+    /**
+     * @param {HTMLCanvasElement} canvas
+     * @param {string} color
+     * @returns {void}
+     * @throws {Error}
+     */
+    constructor(canvas, color, showGrid, gridSize) {
+        /**
+         * @type {HTMLCanvasElement}
+         */
+        this.canvas = canvas;
+        this.canvas.height = 480;
+        this.canvas.width = 480;
+        /**
+         * @type {CanvasRenderingContext2D}
+         */
+        this.ctx = this.canvas.getContext('2d');
+        /**
+         * @type {Array<{x: number, y: number}>}
+         */
+        this.points = [];
+        /**
+         * @type {Array<{points: Array<{x: number, y: number}>, color: string, type: string}>}
+         */
+        this.drawings = [];
+        this.isDrawing = true;
+        this.isGridVisible = showGrid;
+        this.gridSize = gridSize;
+        this.gridColor = '#ddd';
+        this.crossIconSize = 10;
+        this.clickThreshold = 20;
+        this.drawType = 'polygon';
+        this.color = color ?? '#000';
+        this.#init();
     }
 
-    for (let y = 0; y <= canvas.height; y += gridSize) {
-        ctx.moveTo(0, y);
-        ctx.lineTo(canvas.width, y);
-    }
+    #init() {
+        this.canvas.style.cursor = 'crosshair';
+        this.#redraw();
+        this.canvas.addEventListener('click', (event) => {
+            const rect = canvas.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
 
-    ctx.strokeStyle = '#ddd';
-    ctx.stroke();
-}
 
-/**
- * @param {{points: Array<{x: number, y: number}>, color: string}} polygon
- * @returns {void}
- */
-function drawPolygon(polygon) {
-    const points = polygon.points;
-    const color = polygon.color;
-
-    if (points.length < 2) return;
-
-    ctx.beginPath();
-    ctx.moveTo(points[0].x, points[0].y);
-
-    for (let i = 1; i < points.length; i++) {
-        ctx.lineTo(points[i].x, points[i].y);
-    }
-
-    ctx.closePath();
-    ctx.fillStyle = color;
-    ctx.fill();
-
-    const topRight = points.reduce((prev, curr) => (curr.x > prev.x ? curr : prev), points[0]);
-    drawCrossIcon(topRight.x, topRight.y, color);
-}
-
-function drawCrossIcon(x, y, color) {
-    ctx.beginPath();
-    ctx.arc(x, y, CROSS_ICON_SIZE, 0, 2 * Math.PI);
-    ctx.fillStyle = 'white';
-    ctx.fill();
-    ctx.strokeStyle = color;
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(x - CROSS_ICON_SIZE / 2, y - CROSS_ICON_SIZE / 2);
-    ctx.lineTo(x + CROSS_ICON_SIZE / 2, y + CROSS_ICON_SIZE / 2);
-    ctx.moveTo(x + CROSS_ICON_SIZE / 2, y - CROSS_ICON_SIZE / 2);
-    ctx.lineTo(x - CROSS_ICON_SIZE / 2, y + CROSS_ICON_SIZE / 2);
-    ctx.stroke();
-}
-
-canvas.addEventListener('click', (event) => {
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-
-    if (isDrawing) {
-        if (points.length > 0 && Math.abs(x - points[0].x) < CLICK_THRESHOLD && Math.abs(y - points[0].y) < CLICK_THRESHOLD) {
-            polygons.push({ points, color: colorPicker.value });
-            isDrawing = false;
-            points = [];
-            redraw();
-            drawButton.disabled = false;
-            cancelButton.disabled = true;
-            canvas.style.cursor = 'default';
-        } else {
-            points.push({ x, y });
-            if (points.length > 1) {
-                ctx.beginPath();
-                ctx.moveTo(points[points.length - 2].x, points[points.length - 2].y);
-                ctx.lineTo(x, y);
-                ctx.stroke();
+            for (let i = 0; i < this.drawings.length; i++) {
+                const points = this.drawings[i].points;
+                const topRight = points.reduce((prev, curr) => (curr.x > prev.x ? curr : prev), this.drawings[i].points[0]);
+                if (Math.abs(x - topRight.x) < this.clickThreshold && Math.abs(y - topRight.y) < this.clickThreshold) {
+                    this.drawings.splice(i, 1);
+                    this.#redraw();
+                    return;
+                }
             }
+
+            if (this.points.length > 0 && Math.abs(x - this.points[0].x) < this.clickThreshold && Math.abs(y - this.points[0].y) < this.clickThreshold) {
+                this.drawings.push({ points: this.points, color: this.color });
+                this.points = [];
+                this.#redraw();
+            } else {
+                this.points.push({ x, y });
+                if (this.points.length > 1) {
+                    this.ctx.beginPath();
+                    this.ctx.moveTo(this.points[this.points.length - 2].x, this.points[this.points.length - 2].y);
+                    this.ctx.lineTo(x, y);
+                    this.ctx.stroke();
+                }
+            }
+        });
+
+        this.canvas.addEventListener('mousemove', (event) => {
+            const rect = canvas.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+
+            let cursorStyle = 'crosshair';
+
+            for (let i = 0; i < this.drawings.length; i++) {
+                const points = this.drawings[i].points;
+                const topRight = points.reduce((prev, curr) => (curr.x > prev.x ? curr : prev), this.drawings[i].points[0]);
+                if (Math.abs(x - topRight.x) < this.clickThreshold && Math.abs(y - topRight.y) < this.clickThreshold) {
+                    cursorStyle = 'pointer';
+                    break;
+                }
+            }
+
+            this.canvas.style.cursor = cursorStyle;
+
+            if (this.points.length === 0) return;
+
+            this.#redraw();
+
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.points[this.points.length - 1].x, this.points[this.points.length - 1].y);
+            this.ctx.lineTo(x, y);
+            this.ctx.lineWidth = 2;
+            this.ctx.strokeStyle = this.color;
+            this.ctx.stroke();
+        });
+    }
+
+    /**
+     * @param {string} width
+     * @param {string} height
+     * @returns {void}
+     */
+    setCanvasSize(width, height) {
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.#redraw();
+    }
+
+    /**
+     * @param {number} size
+     * @returns {void}
+     */
+    setGridSize(size) {
+        this.gridSize = size;
+        this.#redraw();
+    }
+
+    /**
+     * @param {boolean} isVisible
+     * @returns {void}
+     */
+    toggleGrid(isVisible) {
+        this.isGridVisible = isVisible;
+        this.#redraw();
+    }
+
+    /**
+     * @param {string} color
+     * @returns {void}
+     */
+    setGridColor(color) {
+        this.gridColor = color;
+        this.#redraw();
+    }
+
+    /**
+     * @param {string} color
+     * @returns {void}
+     */
+    setColor(color) {
+        this.color = color;
+    }
+
+    /**
+     * @param {'polygon'|'rectangle'} type
+     * @returns {void}
+     * @throws {Error}
+     */
+    setDrawType(type) {
+        if (type !== 'polygon') {
+            throw new Error('Invalid draw type');
         }
-    } else {
-        for (let i = 0; i < polygons.length; i++) {
-            const points = polygons[i].points;
-            const topRight = points.reduce((prev, curr) => (curr.x > prev.x ? curr : prev), polygons[i].points[0]);
-            if (Math.abs(x - topRight.x) < CROSS_ICON_SIZE && Math.abs(y - topRight.y) < CROSS_ICON_SIZE) {
-                polygons.splice(i, 1);
-                redraw();
-                return;
-            }
-        }
+        this.drawType = type;
     }
-});
 
-canvas.addEventListener('mousemove', (event) => {
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+    /**
+     * @returns {void}
+     * @throws {Error}
+     */
+    #drawGrid() {
+        if (!this.isGridVisible) return;
+        if (this.gridSize <= 0) throw new Error('Grid size must be greater than 0');
 
-    if (isDrawing) {
-        if (points.length === 0) return;
-
-        redraw();
-
-        ctx.beginPath();
-        ctx.moveTo(points[points.length - 1].x, points[points.length - 1].y);
-        ctx.lineTo(x, y);
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = colorPicker.value;
-        ctx.stroke();
-    } else {
-        let cursorStyle = 'default';
-
-        for (let i = 0; i < polygons.length; i++) {
-            const points = polygons[i].points;
-            const topRight = points.reduce((prev, curr) => (curr.x > prev.x ? curr : prev), polygons[i].points[0]);
-            if (Math.abs(x - topRight.x) < CROSS_ICON_SIZE && Math.abs(y - topRight.y) < CROSS_ICON_SIZE) {
-                cursorStyle = 'pointer';
-                break;
-            }
+        this.ctx.beginPath();
+        for (let x = 0; x <= this.canvas.width; x += this.gridSize) {
+            this.ctx.moveTo(x, 0);
+            this.ctx.lineTo(x, this.canvas.height);
         }
 
-        canvas.style.cursor = cursorStyle;
-    }
-});
+        for (let y = 0; y <= this.canvas.height; y += this.gridSize) {
+            this.ctx.moveTo(0, y);
+            this.ctx.lineTo(this.canvas.width, y);
+        }
 
-function redraw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (isGridVisible) {
-        const gridSize = parseInt(gridSizeSelect.value);
-        drawGrid(gridSize);
+        this.ctx.strokeStyle = this.gridColor;
+        this.ctx.stroke();
     }
 
-    polygons.forEach(drawPolygon);
+    /**
+     * @returns {void}
+     */
+    #redraw() {
+        this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.#drawGrid();
 
-    if (points.length > 1) {
-        ctx.beginPath();
+        this.drawings.forEach(this.#drawPolygon.bind(this));
+
+        if (this.points.length > 1) {
+            this.ctx.beginPath();
+            for (let i = 1; i < this.points.length; i++) {
+                this.ctx.moveTo(this.points[i - 1].x, this.points[i - 1].y);
+                this.ctx.lineTo(this.points[i].x, this.points[i].y);
+            }
+            this.ctx.strokeStyle = this.color;
+            this.ctx.stroke();
+        }
+    }
+
+    /**
+     * @param {{points: Array<{x: number, y: number}>, color: string}} polygon
+     * @returns {void}
+     */
+    #drawPolygon(polygon) {
+        const points = polygon.points;
+        const color = polygon.color;
+
+        if (points.length < 2) return;
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(points[0].x, points[0].y);
+
         for (let i = 1; i < points.length; i++) {
-            ctx.moveTo(points[i - 1].x, points[i - 1].y);
-            ctx.lineTo(points[i].x, points[i].y);
+            this.ctx.lineTo(points[i].x, points[i].y);
         }
-        ctx.strokeStyle = colorPicker.value;
-        ctx.stroke();
+
+        this.ctx.closePath();
+        this.ctx.fillStyle = color;
+        this.ctx.fill();
+
+        const topRight = points.reduce((prev, curr) => (curr.x > prev.x ? curr : prev), points[0]);
+        this.#drawCrossIcon(topRight.x, topRight.y, color);
+    }
+
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @param {string} color
+     * @returns {void}
+     */
+    #drawCrossIcon(x, y, color) {
+        this.ctx.beginPath();
+        this.ctx.arc(x, y, this.crossIconSize, 0, 2 * Math.PI);
+        this.ctx.fillStyle = 'white';
+        this.ctx.fill();
+        this.ctx.strokeStyle = color;
+        this.ctx.stroke();
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(x - this.crossIconSize / 2, y - this.crossIconSize / 2);
+        this.ctx.lineTo(x + this.crossIconSize / 2, y + this.crossIconSize / 2);
+        this.ctx.moveTo(x + this.crossIconSize / 2, y - this.crossIconSize / 2);
+        this.ctx.lineTo(x - this.crossIconSize / 2, y + this.crossIconSize / 2);
+        this.ctx.stroke();
     }
 }
