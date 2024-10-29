9af6de8e1437592a89411dfbbf8d7360b3edf9d4
Momin Ahmad <momin.ahmad-ext@jci.com>
2024-10-29 14:35:05 +0530

    added docs

--- main.js
+++ main.js
@@ -9,167 +9,206 @@ import CrossIcon from './shapes/cross-icon.js';
 import Freehand from './shapes/freehand.js';
 
 /**
+ * Represents a point in 2D space.
  * @typedef {Object} Point
- * @property {number} x
- * @property {number} y
+ * @property {number} x - The x-coordinate of the point.
+ * @property {number} y - The y-coordinate of the point.
  */
 
 /**
+ * Represents a drawing on the canvas.
  * @typedef {Object} Drawing
- * @property {Array<Point>} points
- * @property {string} color
- * @property {DrawingType} type
- * @property {number} [radius]
- * @property {number} [lineWidth]
- * @property {CanvasSize} canvasSize
+ * @property {Array<Point>} points - The points that make up the drawing.
+ * @property {string} color - The color of the drawing.
+ * @property {DrawingType} type - The type of the drawing.
+ * @property {number} [radius] - The radius of the drawing (only for circle).
+ * @property {number} [lineWidth] - The line width of the drawing (only for line and freehand).
+ * @property {CanvasSize} canvasSize - The size of the canvas when the drawing was created.
  */
 
 /**
+ * Represents the size of the canvas.
  * @typedef {Object} CanvasSize
- * @property {number} width
- * @property {number} height
+ * @property {number} width - The width of the canvas.
+ * @property {number} height - The height of the canvas.
  */
 
 /**
+ * The type of drawing.
  * @typedef {'polygon'|'rectangle'|'circle'|'triangle'|'line'|'freehand'} DrawingType
+ * @property {'polygon'} polygon - Type for drawing polygons.
+ * @property {'rectangle'} rectangle - Type for drawing rectangles.
+ * @property {'circle'} circle - Type for drawing circles.
+ * @property {'triangle'} triangle - Type for drawing triangles.
+ * @property {'line'} line - Type for drawing lines.
+ * @property {'freehand'} freehand - Type for drawing freehand shapes.
  */
 
 /**
- * @typedef {'draw'|'move'|'resize'} DrawingMode
+ * The mode of drawing.
+ * @typedef {'draw'|'move'|'resize'|'view'} DrawingMode
+ * @property {'draw'} draw - Mode for drawing shapes.
+ * @property {'move'} move - Mode for moving shapes.
+ * @property {'resize'} resize - Mode for resizing shapes.
+ * @property {'view'} view - Mode for viewing shapes without editing.
  */
 
 /**
+ * Options for drawing shapes on the canvas.
  * @typedef {Object} DrawCanvasShapesOptions
- * @property {HTMLCanvasElement} canvas
- * @property {CanvasSize} [canvasSize]
- * @property {string} [gridColor]
- * @property {number} [gridSize]
- * @property {boolean} [showGrid]
- * @property {Array<Drawing>} [drawings]
- * @property {DrawingType} [drawingType]
- * @property {string} [drawingColor]
- * @property {DrawingMode} [drawingMode]
- * @property {number} [crossIconSize]
- * @property {boolean} [showCrossIcon]
- * @property {number} [clickThreshold]
- * @property {boolean} [resizeOnCanvasSizeChange]
- * @property {number} [lineWidth]
+ * @property {HTMLCanvasElement} canvas - The canvas element to draw shapes on.
+ * @property {CanvasSize} [canvasSize] - The size of the canvas.
+ * @property {string} [gridColor] - The color of the grid.
+ * @property {number} [gridSize] - The size of the grid.
+ * @property {boolean} [showGrid] - Wether to show the grid or not.
+ * @property {Array<Drawing>} [drawings] - The drawings to display on the canvas.
+ * @property {DrawingType} [drawingType] - The type of drawing to draw.
+ * @property {string} [drawingColor] - The color of the drawing.
+ * @property {DrawingMode} [drawingMode] - The mode of drawing.
+ * @property {number} [crossIconSize] - The size of the cross icon.
+ * @property {boolean} [showCrossIcon] - Wether to show the cross icon or not.
+ * @property {number} [clickThreshold] - The threshold for detecting final click in polygons.
+ * @property {boolean} [resizeOnCanvasSizeChange] - Wether to resize the drawings when the canvas size changes.
+ * @property {number} [lineWidth] - The line width of the drawing (only for line and freehand).
  */
 
 export class DrawCanvasShapes {
     /**
+     * The canvas element to draw shapes on.
      * @type {HTMLCanvasElement}
      */
     #canvas;
 
     /**
+     * The 2D context of the canvas.
      * @type {CanvasRenderingContext2D}
      */
     #ctx;
 
     /**
+     * The points of the current drawing.
      * @type {Array<Point>}
      */
     #points = [];
 
     /**
+     * The drawings to display on the canvas.
      * @type {Array<Drawing>}
      */
     #drawings;
 
     /**
+     * Wether to show the grid or not.
      * @type {boolean}
      */
     #showGrid;
 
     /**
+     * The size of the grid.
      * @type {number}
      */
     #gridSize;
 
     /**
+     * The color of the grid.
      * @type {string}
      */
     #gridColor;
 
     /**
+     * The size of the cross icon.
      * @type {number}
      */
     #crossIconSize;
 
     /**
+     * The threshold for detecting final click in polygons.
      * @type {number}
      */
     #clickThreshold;
 
     /**
+     * The type of drawing to draw.
      * @type {DrawingType}
      */
     #drawingType;
 
     /**
+     * The color of the drawing.
      * @type {string}
      */
     #drawingColor;
 
     /**
+     * The mode of drawing.
      * @type {DrawingMode}
      */
     #drawingMode;
 
     /**
+     * Wether to show the cross icon or not.
      * @type {boolean}
      */
     #showCrossIcon;
 
     /**
+     * The cross icon to show on the canvas.
      * @type {CrossIcon | undefined}
      */
     #crossIcon;
 
     /**
+     * The index of the drawing being moved or resized.
      * @type {number | undefined}
      */
     #movingDrawingIndex;
 
     /**
+     * The index of the point being resized.
      * @type {number | undefined}
      */
     #resizingPointIndex;
 
     /**
+     * The point from which the drawing is being moved or resized.
      * @type {Point | undefined}
      */
     #movingStartPoint;
 
     /**
+     * Drawing handlers for all different types of drawings except freehand.
      * @type {Object<string, Polygon|Rectangle|Circle|Triangle|Line> | undefined}
      */
     #drawingHandlers;
 
     /**
+     * Wether a freehand drawing is in progress.
      * @type {boolean | undefined}
      */
     #freehandInProgress = false;
 
     /**
+     * Wether to resize the drawings when the canvas size changes.
      * @type {boolean}
      */
     #resizeOnCanvasSizeChange;
 
     /**
+     * The freehand drawing handler.
      * @type {Freehand | undefined}
      */
     #freehand;
 
     /**
+     * The line width of the drawing (only for line and freehand).
      * @type {number}
      */
     #lineWidth;
 
     /**
-     * @param {DrawCanvasShapesOptions} options
-     * @throws {Error}
+     * Creates a new instance of DrawCanvasShapes.
+     * @param {DrawCanvasShapesOptions} options - The options for drawing shapes on the canvas.
+     * @throws {Error} - If the canvas element is not provided.
      */
     constructor({
         canvas,
@@ -213,12 +252,18 @@ export class DrawCanvasShapes {
         this.#init();
     }
 
+    /**
+     * Initializes the canvas
+     */
     #init() {
         this.#initializeShapes();
         this.#redraw();
         this.#initializeEventListeners();
     }
 
+    /**
+     * Initializes the drawing handlers for all different types of drawings.
+     */
     #initializeShapes() {
         this.#drawingHandlers = {
             polygon: new Polygon(this.#ctx, this.#clickThreshold),
@@ -231,6 +276,9 @@ export class DrawCanvasShapes {
         this.#crossIcon = new CrossIcon(this.#ctx, this.#crossIconSize, this.#showCrossIcon);
     }
 
+    /**
+     * Initializes the event listeners on the canvas.
+     */
     #initializeEventListeners() {
         this.#canvas.onclick = this.#canvasClick;
         this.#canvas.onmouseenter = this.#canvasMouseEnter;
@@ -241,7 +289,8 @@ export class DrawCanvasShapes {
     }
 
     /**
-     * @param {MouseEvent} event
+     * Handles the click event on the canvas.
+     * @param {MouseEvent} event - The mouse event.
      */
     #canvasClick = (event) => {
         if (this.#drawingMode !== 'draw') return;
@@ -261,12 +310,16 @@ export class DrawCanvasShapes {
         this.#redraw();
     }
 
+    /**
+     * Handles the mouse enter event on the canvas.
+     */
     #canvasMouseEnter = () => {
         this.#canvas.style.cursor = this.#drawingMode === 'draw' ? 'crosshair' : 'default';
     }
 
     /**
-     * @param {MouseEvent} event
+     * Handles the mouse down event on the canvas.
+     * @param {MouseEvent} event - The mouse event.
      */
     #canvasMouseDown = (event) => {
         if (this.#drawingMode === 'draw' && this.#drawingType === 'freehand') {
@@ -302,7 +355,8 @@ export class DrawCanvasShapes {
     }
 
     /**
-     * @param {MouseEvent} event
+     * Handles the mouse move event on the canvas.
+     * @param {MouseEvent} event - The mouse event.
      */
     #canvasMouseMove = (event) => {
         const { x, y } = this.#getMousePosition(event);
@@ -313,8 +367,9 @@ export class DrawCanvasShapes {
     }
 
     /**
-     * @param {number} x
-     * @param {number} y
+     * Handles the mouse move event when in draw mode.
+     * @param {number} x - The x-coordinate of the mouse.
+     * @param {number} y - The y-coordinate of the mouse.
      */
     #drawModeMouseMove(x, y) {
         if (this.#points.length === 0) {
@@ -333,8 +388,9 @@ export class DrawCanvasShapes {
     }
 
     /**
-     * @param {number} x
-     * @param {number} y
+     * Handles the mouse move event when in move mode.
+     * @param {number} x - The x-coordinate of the mouse.
+     * @param {number} y - The y-coordinate of the mouse.
      */
     #moveModeMouseMove(x, y) {
         if (this.#movingDrawingIndex === undefined || this.#movingStartPoint === undefined) {
@@ -355,8 +411,9 @@ export class DrawCanvasShapes {
     }
 
     /**
-     * @param {number} x
-     * @param {number} y
+     * Handles the mouse move event when in resize mode.
+     * @param {number} x - The x-coordinate of the mouse.
+     * @param {number} y - The y-coordinate of the mouse.
      */
     #resizeModeMouseMove(x, y) {
         if (this.#movingDrawingIndex === undefined || this.#resizingPointIndex === undefined || this.#movingStartPoint === undefined) {
@@ -390,6 +447,9 @@ export class DrawCanvasShapes {
         }
     }
 
+    /**
+     * Resets the move and resize state.
+     */
     #resetMoveResizeState = () => {
         this.#movingDrawingIndex = undefined;
         this.#resizingPointIndex = undefined;
@@ -415,18 +475,20 @@ export class DrawCanvasShapes {
     }
 
     /**
-     * @param {Drawing} drawing
-     * @param {Point} point
-     * @returns {boolean}
+     * Checks if a mouse point is inside a drawing
+     * @param {Drawing} drawing - The drawing to check.
+     * @param {Point} point - The mouse point to check.
+     * @returns {boolean} - Wether the mouse point is inside the drawing.
      */
     #isPointInside(drawing, { x, y }) {
         return this.#drawingHandlers?.[drawing.type]?.isPointInside(drawing, { x, y }) ?? false;
     }
 
     /**
-     * @param {Drawing} drawing
-     * @param {Point} point
-     * @returns {number}
+     * Checks if a mouse point is on a point of a drawing
+     * @param {Drawing} drawing - The drawing to check.
+     * @param {Point} point - The mouse point to check.
+     * @returns {number} - The index of the point the mouse point is on if any, otherwise -1.
      */
     #isPointOnPoint(drawing, { x, y }) {
         if (['triangle', 'line'].includes(drawing.type)) {
@@ -436,8 +498,9 @@ export class DrawCanvasShapes {
     }
 
     /**
-     * @param {MouseEvent} event
-     * @returns {Point}
+     * Gets the mouse position relative to the canvas.
+     * @param {MouseEvent} event - The mouse event.
+     * @returns {Point} - The mouse position relative to the canvas.
      */
     #getMousePosition(event) {
         const rect = this.#canvas.getBoundingClientRect();
@@ -447,6 +510,10 @@ export class DrawCanvasShapes {
         };
     }
 
+    /**
+     * Draws the grid on the canvas.
+     * @throws {Error} - If the grid size is less than or equal to 0.
+     */
     #drawGrid() {
         if (!this.#showGrid) return;
         if (this.#gridSize <= 0) throw new Error('Grid size must be greater than 0');
@@ -467,6 +534,9 @@ export class DrawCanvasShapes {
         this.#ctx.stroke();
     }
 
+    /**
+     * Redraws the canvas.
+     */
     #redraw() {
         this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
         this.#drawGrid();
@@ -490,6 +560,9 @@ export class DrawCanvasShapes {
         }
     }
 
+    /**
+     * Draws all the shapes on the canvas.
+     */
     #drawShapes() {
         this.#drawings.forEach((drawing) => {
             if (this.#resizeOnCanvasSizeChange) {
@@ -520,64 +593,72 @@ export class DrawCanvasShapes {
     // Public setters
 
     /**
-     * @param {CanvasSize} canvasSize
+     * Sets the size of the canvas.
+     * @param {CanvasSize} canvasSize - The new size of the canvas.
      */
-    set canvasSize({ width, height }) {
+    setCanvasSize({ width, height }) {
         this.#canvas.width = width;
         this.#canvas.height = height;
         this.#redraw();
     }
 
     /**
-     * @param {number} gridSize
+     * Sets the color of the grid.
+     * @param {number} gridSize - The new size of the grid.
      */
-    set gridSize(gridSize) {
+    setGridSize(gridSize) {
         this.#gridSize = gridSize;
         this.#redraw();
     }
 
     /**
-     * @param {boolean} showGrid
+     * Sets weather to show the grid or not.
+     * @param {boolean} showGrid - Weather to show the grid.
      */
-    set showGrid(showGrid) {
+    setShowGrid(showGrid) {
         this.#showGrid = showGrid;
         this.#redraw();
     }
 
     /**
-     * @param {Array<Drawing>} drawings
+     * Sets the drawings to display on the canvas
+     * @param {Array<Drawing>} drawings - The new drawings to display.
      */
-    set drawings(drawings) {
+    setDrawings(drawings) {
         this.#drawings = drawings;
         this.#redraw();
     }
 
     /**
-     * @param {DrawingType} drawingType
+     * Sets the type of drawing to draw.
+     * @param {DrawingType} drawingType - The new type of drawing.
      */
-    set drawingType(drawingType) {
+    setDrawingType(drawingType) {
         this.#drawingType = drawingType;
     }
 
     /**
-     * @param {string} drawingColor
+     * Sets the color of the drawing.
+     * @param {string} drawingColor - The new color of the drawing.
      */
-    set drawingColor(drawingColor) {
+    setDrawingColor(drawingColor) {
         this.#drawingColor = drawingColor;
     }
 
     /**
-     * @param {DrawingMode} drawingMode
+     * Sets the mode of drawing.
+     * @param {DrawingMode} drawingMode - The new mode of drawing.
      */
-    set drawingMode(drawingMode) {
+    setDrawingMode(drawingMode) {
         this.#drawingMode = drawingMode;
         this.#redraw();
     }
 
     /**
-     * @param {boolean} showCrossIcon
+     * Sets weather to show the cross icon or not.
+     * @param {boolean} showCrossIcon - Weather to show the cross icon.
      */
-    set showCrossIcon(showCrossIcon) {
+    setShowCrossIcon(showCrossIcon) {
         this.#showCrossIcon = showCrossIcon;
         if (this.#crossIcon)
             this.#crossIcon.showCrossIcon = showCrossIcon;
@@ -585,28 +666,36 @@ export class DrawCanvasShapes {
     }
 
     /**
-     * @param {number} lineWidth
+     * Sets the width of the lines in the drawing (only for line and freehand).
+     * @param {number} lineWidth - The new width of the lines.
      */
-    set lineWidth(lineWidth) {
+    setLineWidth(lineWidth) {
         this.#lineWidth = lineWidth;
     }
 
     // Public getters
 
     /**
+     * Gets the current drawings on the canvas.
      * @returns {Array<Drawing>}
      */
-    get drawings() {
+    getDrawings() {
         return this.#drawings;
     }
 
     // Public methods
 
+    /**
+     * Cancels the current drawing operation.
+     */
     cancelDrawing() {
         this.#points = [];
         this.#redraw();
     }
 
+    /**
+     * Clears the canvas and removes all drawings.
+     */
     clearCanvas() {
         this.#points = [];
         this.#drawings = [];
